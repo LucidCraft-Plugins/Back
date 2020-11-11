a94bf72e0f30e0bde81f60a48e817e334df929ef
minijaham <60860894+minijaham@users.noreply.github.com>
2020-11-10 18:37:15 -0800

    Update Main.php

--- src/minijaham/Back/Main.php
+++ src/minijaham/Back/Main.php
@@ -1,11 +1,15 @@
-<?php
-namespace minijaham\Back;
+<?php
+
+namespace minijaham\Back;
+
 use minijaham\Back\Commands\Back;
 use minijaham\Back\EventListener;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as C;
-class Main extends PluginBase implements Listener {
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase implements Listener{
+
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getServer()->getCommandMap()->register("back", new Back($this));
